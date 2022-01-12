3c77720e7322e837bc334ae87bbd4ca90f484d11
lotrbuilders <daan.2008@hotmail.com>
2022-01-12 15:33:34 +0100

    Add is_target and outside_class [register-class]

--- src/backend/register_allocation/register_class.rs
+++ src/backend/register_allocation/register_class.rs
@@ -1,12 +1,8 @@
 use super::RegisterInterface;
 
 use std::fmt::Display;
-//use std::iter::FromIterator;
 use std::marker::PhantomData;
-//use std::ops::BitAnd;
-//use std::ops::BitOr;
-use std::ops::Index;
-//use std::ops::Sub;
+use std::ops::{Index, Not};
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct RegisterClass<R: RegisterInterface> {
@@ -16,6 +12,20 @@ pub struct RegisterClass<R: RegisterInterface> {
 
 #[allow(dead_code)]
 impl<R: RegisterInterface> RegisterClass<R> {
+    pub fn is_target(&self) -> Option<R> {
+        let mut count = 0;
+        let mut result = R::REG_DEFAULT;
+        for reg in 0..self.registers.len() {
+            if self.registers[reg] {
+                result = R::REG_LOOKUP[count];
+                count += 1;
+            }
+            if count >= 1 {
+                return None;
+            }
+        }
+        return Some(result);
+    }
     pub fn iter<'a>(&'a self) -> RegisterClassIterRef<'a, R> {
         self.into_iter()
     }
@@ -49,28 +59,6 @@ impl<R: RegisterInterface> IntoIterator for RegisterClass<R> {
     }
 }
 
-/*
-impl<R: RegisterInterface> FromIterator<R> for RegisterClass {
-    fn from_iter<T: IntoIterator<Item = Register>>(iter: T) -> Self {
-        let mut registers = [false; REG_COUNT];
-        for reg in iter {
-            registers[reg as usize] = true;
-        }
-        RegisterClass { registers }
-    }
-}
-
-impl<'a, R: RegisterInterface> FromIterator<&'a Register> for RegisterClass<R> {
-    fn from_iter<T: IntoIterator<Item = &'a Register>>(iter: T) -> Self {
-        let mut registers = [false; REG_COUNT];
-        for reg in iter {
-            registers[*reg as usize] = true;
-        }
-        RegisterClass { registers }
-    }
-}
-*/
-
 impl<R: RegisterInterface> Index<usize> for RegisterClass<R> {
     type Output = bool;
     fn index(&self, index: usize) -> &Self::Output {
@@ -81,100 +69,9 @@ impl<R: RegisterInterface> Index<usize> for RegisterClass<R> {
 impl<R: RegisterInterface> Index<R> for RegisterClass<R> {
     type Output = bool;
     fn index(&self, index: R) -> &Self::Output {
-        &self[index.into()]
+        &self[<R as Into<usize>>::into(index)]
     }
 }
-
-/*
-impl<R: RegisterInterface> BitOr<&Self> for RegisterClass<R> {
-    type Output = RegisterClass<R>;
-    fn bitor(self, rhs: &Self) -> Self::Output {
-        &self | rhs
-    }
-}
-
-impl<R: RegisterInterface> BitOr for RegisterClass<R> {
-    type Output = RegisterClass<R>;
-    fn bitor(self, rhs: Self) -> Self::Output {
-        &self | &rhs
-    }
-}
-
-/// Or represents the union
-impl<R: RegisterInterface> BitOr for &RegisterClass<R> {
-    type Output = RegisterClass<R>;
-    fn bitor(self, rhs: Self) -> Self::Output {
-        (0..REG_COUNT)
-            .filter_map(|i| {
-                if self.registers[i] || rhs.registers[i] {
-                    Some(REG_LOOKUP[i])
-                } else {
-                    None
-                }
-            })
-            .collect()
-    }
-}
-
-impl<R: RegisterInterface> BitAnd<&Self> for RegisterClass {
-    type Output = RegisterClass;
-    fn bitand(self, rhs: &Self) -> Self::Output {
-        &self & rhs
-    }
-}
-
-impl<R: RegisterInterface> BitAnd for RegisterClass {
-    type Output = RegisterClass;
-    fn bitand(self, rhs: Self) -> Self::Output {
-        &self & &rhs
-    }
-}
-
-/// And represents the intersection
-impl<R: RegisterInterface> BitAnd for &RegisterClass {
-    type Output = RegisterClass;
-    fn bitand(self, rhs: Self) -> Self::Output {
-        (0..R::REG_COUNT)
-            .filter_map(|i| {
-                if self.registers[i] && rhs.registers[i] {
-                    Some(REG_LOOKUP[i])
-                } else {
-                    None
-                }
-            })
-            .collect()
-    }
-}
-
-impl<R: RegisterInterface> Sub<&Self> for RegisterClass {
-    type Output = RegisterClass;
-    fn sub(self, rhs: &Self) -> Self::Output {
-        &self - rhs
-    }
-}
-
-impl<R: RegisterInterface> Sub for RegisterClass {
-    type Output = RegisterClass;
-    fn sub(self, rhs: Self) -> Self::Output {
-        &self - &rhs
-    }
-}
-
-/// Subtract represents the complement
-impl<R: RegisterInterface> Sub for &RegisterClass {
-    type Output = RegisterClass;
-    fn sub(self, rhs: Self) -> Self::Output {
-        (0..R::REG_COUNT)
-            .filter_map(|i| {
-                if self.registers[i] && !rhs.registers[i] {
-                    Some(REG_LOOKUP[i])
-                } else {
-                    None
-                }
-            })
-            .collect()
-    }
-}*/
 
 impl<R: RegisterInterface + Display> Display for RegisterClass<R> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -215,10 +112,56 @@ impl<R: RegisterInterface> Iterator for RegisterClassIter<R> {
     fn next(&mut self) -> Option<Self::Item> {
         while self.index < R::REG_COUNT {
             let result = Some(R::REG_LOOKUP[self.index].clone());
-            self.index += 1;
             if self.class.registers[self.index] {
+                self.index += 1;
                 return result;
             }
+            self.index += 1;
+        }
+        None
+    }
+}
+
+impl<R: RegisterInterface> Not for RegisterClass<R> {
+    type Output = RegisterClassNot<R>;
+
+    fn not(self) -> Self::Output {
+        RegisterClassNot { class: self }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct RegisterClassNot<R: RegisterInterface> {
+    class: RegisterClass<R>,
+}
+
+impl<R: RegisterInterface> IntoIterator for RegisterClassNot<R> {
+    type Item = R;
+    type IntoIter = RegisterClassNotIter<R>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        RegisterClassNotIter {
+            class: self.class,
+            index: 0,
+        }
+    }
+}
+
+pub struct RegisterClassNotIter<R: RegisterInterface> {
+    class: RegisterClass<R>,
+    index: usize,
+}
+
+impl<R: RegisterInterface> Iterator for RegisterClassNotIter<R> {
+    type Item = R;
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.index < R::REG_COUNT {
+            let result = Some(R::REG_LOOKUP[self.index].clone());
+            if !self.class.registers[self.index] {
+                self.index += 1;
+                return result;
+            }
+            self.index += 1;
         }
         None
     }
