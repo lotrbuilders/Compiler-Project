004baf61162b938b534ffce51386b31d4dd402df
lotrbuilders <daan.2008@hotmail.com>
2022-01-27 11:00:09 +0100

    Improve formatting [x86-64]

--- src/backend/amd64/mod.rs
+++ src/backend/amd64/mod.rs
@@ -34,48 +34,46 @@ rburg::rburg_main! {
 :       Store i32(Imm(#i),a adr)        "\tmov dword[{a}],{i}\n"
 :       Store Pi64(Imm(#i),a adr)       "\tmov qword[{a}],{i}\n"
 :       Store Pi64(Imm(#i),a %ireg)     "\tmov qword[{a:.64}],{i}\n"
-:       Label(#i)                    ".L{i}:\n"
-%ireg:  Label(#i)                    ".L{i}:\n"
-:       Jmp(#i)                      "\tjmp .L{i}\n" {2}
-:       Jmp(#i)                      "\t;jmp .L{}\n" {self.empty_jump_target(index)}
-:       Jcc pi64i32(r %ireg,#l)      "\ttest {r},{r}\n\tjnz .L{l}\n" {2}
-:       Jnc pi64i32(r %ireg,#l)      "\ttest {r},{r}\n\tjz .L{l}\n"  {2}
+:       Label(#i)                       ".L{i}:\n"
+%ireg:  Label(#i)                       ".L{i}:\n"
+:       Jmp(#i)                         "\tjmp .L{i}\n" {2}
+:       Jmp(#i)                         "\t;jmp .L{}\n" {self.empty_jump_target(index)}
+:       Jcc pi64i32(r %ireg,#l)         "\ttest {r},{r}\n\tjnz .L{l}\n" {2}
+:       Jnc pi64i32(r %ireg,#l)         "\ttest {r},{r}\n\tjz .L{l}\n"  {2}
 
-scale:  Imm i32i64(#i)              "{i}" {self.scale(index)}
-con:    Imm i32i64(#i)              "{i}"
-adr:    AddrL(#a)                   "rbp+{a}"
-adr:    AddrG(#a)                   "{a}"
+scale:  Imm i32i64(#i)                  "{i}" {self.scale(index)}
+con:    Imm i32i64(#i)                  "{i}"
+adr:    AddrL(#a)                       "rbp+{a}"
+adr:    AddrG(#a)                       "{a}"
 adr:    Add p(a %ireg,  Mul s64(r %ireg,i scale))  "{a:.64}+{i}*{r:.64}"
-adr:    Add p(a %ireg,  r %ireg)    "{a:.64}+{r:.64}"
-mem:    Load(a adr)                 "[{a}]"
-mem:    Load(r %ireg)               "[{r:.64}]"
-acon:   i con                       "{i}"
-acon:   a adr                       "{a}"
-mcon:   i con                       "{i}"
-mcon:   m mem                       "{m}"
+adr:    Add p(a %ireg,  r %ireg)        "{a:.64}+{r:.64}"
+mem:    Load(a adr)                     "[{a}]"
+mem:    Load(r %ireg)                   "[{r:.64}]"
+mcon:   i con                           "{i}"
+mcon:   m mem                           "{m}"
 
-mem64:  Load pi64(a adr)            "[{a}]"
-mem64:  Load pi64(r %ireg)          "[{r:.64}]"
-mcon64:  i con                      "{i}"
-mcon64:  m mem64                    "{m}"
+mem64:  Load pi64(a adr)    "[{a}]"
+mem64:  Load pi64(r %ireg)  "[{r:.64}]"
+mcon64:  i con              "{i}"
+mcon64:  m mem64            "{m}"
 
-%ireg:  m mcon                      "\tmov {res}, {m}\n"          {1}
-%ireg:  m mcon64                    "\tmov {res:.64}, {m}\n"      {1}
-%ireg:  a adr                       "\tlea {res:.64}, [{a}]\n"    {1}
+%ireg:  m mcon              "\tmov {res}, {m}\n"          {1}
+%ireg:  m mcon64            "\tmov {res:.64}, {m}\n"      {1}
+%ireg:  a adr               "\tlea {res:.64}, [{a}]\n"    {1}
 
-%ireg:  Imm pi64(#i)                "\tmov {res:.64}, {i}\n"
+%ireg:  Imm pi64(#i)        "\tmov {res:.64}, {i}\n"
 
-%ireg: Load i8(a adr)               "\tmov {res:.8}, [{a}]\n"
-%ireg: Load i8(a %ireg)             "\tmov {res:.8}, [{a:.64}]\n"
-%ireg: Load i16(a adr)              "\tmov {res:.16}, [{a}]\n"
-%ireg: Load i16(a %ireg)            "\tmov {res:.16}, [{a:.64}]\n"
+%ireg: Load i8(a adr)       "\tmov {res:.8}, [{a}]\n"
+%ireg: Load i8(a %ireg)     "\tmov {res:.8}, [{a:.64}]\n"
+%ireg: Load i16(a adr)      "\tmov {res:.16}, [{a}]\n"
+%ireg: Load i16(a %ireg)    "\tmov {res:.16}, [{a:.64}]\n"
 
-%ireg:  Cvs s64s32(Load s8(a adr))     "\tmovsx {res}, byte [{a}]\n"
-%ireg:  Cvs s64s32(Load s8(r %ireg))   "\tmovsx {res}, byte [{r:.64}]\n"
-%ireg:  Cvs s64s32(Load s16(a adr))    "\tmovsx {res}, word [{a}]\n"
-%ireg:  Cvs s64s32(Load s16(r %ireg))  "\tmovsx {res}, word [{r:.64}]\n"
-%ireg:  Cvs s64(Load s32(a adr))       "\tmovsx {res:.64}, dword [{a}]\n"
-%ireg:  Cvs s64(Load s32(r %ireg))     "\tmovsx {res:.64}, dword [{r:.64}]\n"
+%ireg:  Cvs s64s32(Load s8(a adr))          "\tmovsx {res}, byte [{a}]\n"
+%ireg:  Cvs s64s32(Load s8(r %ireg))        "\tmovsx {res}, byte [{r:.64}]\n"
+%ireg:  Cvs s64s32(Load s16(a adr))         "\tmovsx {res}, word [{a}]\n"
+%ireg:  Cvs s64s32(Load s16(r %ireg))       "\tmovsx {res}, word [{r:.64}]\n"
+%ireg:  Cvs s64(Load s32(a adr))            "\tmovsx {res:.64}, dword [{a}]\n"
+%ireg:  Cvs s64(Load s32(r %ireg))          "\tmovsx {res:.64}, dword [{r:.64}]\n"
 
 %ireg:  Add pi64i32(a %ireg , b %ireg)      ?"\tadd {res}, {b} ; {res} = {a} + {b}\n"   {1}
 
@@ -91,44 +89,44 @@ mcon64:  m mem64                    "{m}"
 %ireg:  Xor i64i32 (a %ireg , b %ireg)      ?"\txor {res}, {b} ; {res} = {a} ^ {b}\n"   {1}
 %ireg:  Xor i64i32 (a %ireg , Imm(#_i))     ?"\tnot {res} ; {res} = ~{a}\n"             {self.range(self.get_right_index(index),-1,-1)+1}
 
-%ireg:  Eq pi64i32(a %ireg , b %ireg)      "\tcmp {a}, {b}\n\tsete {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"    {3}
-%ireg:  Ne pi64i32(a %ireg , b %ireg)      "\tcmp {a}, {b}\n\tsetne {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"   {3}
-%ireg:  Eq pi64i32(a %ireg , Imm(#i))      "\ttest {a}, {a}\n\tsetz {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {i}\n"   {self.range(self.get_right_index(index),0,0)+2}
-%ireg:  Ne pi64i32(a %ireg , Imm(#i))      "\ttest {a}, {a}\n\tsetnz {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {i}\n"  {self.range(self.get_right_index(index),0,0)+2}
+%ireg:  Eq pi64i32(a %ireg , b %ireg)       "\tcmp {a}, {b}\n\tsete {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"    {3}
+%ireg:  Ne pi64i32(a %ireg , b %ireg)       "\tcmp {a}, {b}\n\tsetne {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"   {3}
+%ireg:  Eq pi64i32(a %ireg , Imm(#i))       "\ttest {a}, {a}\n\tsetz {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {i}\n"   {self.range(self.get_right_index(index),0,0)+2}
+%ireg:  Ne pi64i32(a %ireg , Imm(#i))       "\ttest {a}, {a}\n\tsetnz {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {i}\n"  {self.range(self.get_right_index(index),0,0)+2}
 
-%ireg:  Lt s32s64 (a %ireg , b %ireg)  "\tcmp {a}, {b}\n\tsetl {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"       {3}
-%ireg:  Le s32s64 (a %ireg , b %ireg)  "\tcmp {a}, {b}\n\tsetle {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"      {3}
-%ireg:  Gt s32s64 (a %ireg , b %ireg)  "\tcmp {a}, {b}\n\tsetg {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"       {3}
-%ireg:  Ge s32s64 (a %ireg , b %ireg)  "\tcmp {a}, {b}\n\tsetge {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"      {3}
+%ireg:  Lt s32s64 (a %ireg , b %ireg)       "\tcmp {a}, {b}\n\tsetl {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"       {3}
+%ireg:  Le s32s64 (a %ireg , b %ireg)       "\tcmp {a}, {b}\n\tsetle {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"      {3}
+%ireg:  Gt s32s64 (a %ireg , b %ireg)       "\tcmp {a}, {b}\n\tsetg {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"       {3}
+%ireg:  Ge s32s64 (a %ireg , b %ireg)       "\tcmp {a}, {b}\n\tsetge {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"      {3}
 
-%ireg:  Lt p (a %ireg , b %ireg)    "\tcmp {a:.64}, {b:.64}\n\tsetb {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"       {3}
-%ireg:  Le p (a %ireg , b %ireg)    "\tcmp {a:.64}, {b:.64}\n\tsetbe {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"      {3}
-%ireg:  Gt p (a %ireg , b %ireg)    "\tcmp {a:.64}, {b:.64}\n\tseta {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"       {3}
-%ireg:  Ge p (a %ireg , b %ireg)    "\tcmp {a:.64}, {b:.64}\n\tsetae {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"      {3}
+%ireg:  Lt p (a %ireg , b %ireg)            "\tcmp {a:.64}, {b:.64}\n\tsetb {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"       {3}
+%ireg:  Le p (a %ireg , b %ireg)            "\tcmp {a:.64}, {b:.64}\n\tsetbe {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"      {3}
+%ireg:  Gt p (a %ireg , b %ireg)            "\tcmp {a:.64}, {b:.64}\n\tseta {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"       {3}
+%ireg:  Ge p (a %ireg , b %ireg)            "\tcmp {a:.64}, {b:.64}\n\tsetae {res:.8}\n\tmovsx {res},{res:.8}; {res} = {a} == {b}\n"      {3}
 
-:  Jcc(Eq pi64i32(a %ireg , b %ireg),#l)      "\tcmp {a}, {b}\n\tje  .L{l}\n"   {1}
-:  Jcc(Ne pi64i32(a %ireg , b %ireg),#l)      "\tcmp {a}, {b}\n\tjne .L{l}\n"   {1}
-:  Jcc(Eq pi64i32(a %ireg , Imm(#_i)),#l)     "\ttest {a}, {a}\n\tjz  .L{l}\n"  {self.range(self.get_right_index(self.get_left_index(index)),0,0)+1}
-:  Jcc(Ne pi64i32(a %ireg , Imm(#_i)),#l)     "\ttest {a}, {a}\n\tjnz .L{l}\n"  {self.range(self.get_right_index(self.get_left_index(index)),0,0)+1}
+:  Jcc(Eq pi64i32(a %ireg , b %ireg),#l)    "\tcmp {a}, {b}\n\tje  .L{l}\n"   {1}
+:  Jcc(Ne pi64i32(a %ireg , b %ireg),#l)    "\tcmp {a}, {b}\n\tjne .L{l}\n"   {1}
+:  Jcc(Eq pi64i32(a %ireg , Imm(#_i)),#l)   "\ttest {a}, {a}\n\tjz  .L{l}\n"  {self.range(self.get_right_index(self.get_left_index(index)),0,0)+1}
+:  Jcc(Ne pi64i32(a %ireg , Imm(#_i)),#l)   "\ttest {a}, {a}\n\tjnz .L{l}\n"  {self.range(self.get_right_index(self.get_left_index(index)),0,0)+1}
 
-:  Jcc(Lt s32s64 (a %ireg , b %ireg),#l)  "\tcmp {a}, {b}\n\tjl  .L{l}\n"       {1}
-:  Jcc(Le s32s64 (a %ireg , b %ireg),#l)  "\tcmp {a}, {b}\n\tjle .L{l}\n"       {1}
-:  Jcc(Gt s32s64 (a %ireg , b %ireg),#l)  "\tcmp {a}, {b}\n\tjg .L{l}\n"       {1}
-:  Jcc(Ge s32s64 (a %ireg , b %ireg),#l)  "\tcmp {a}, {b}\n\tjge .L{l}\n"       {1}
+:  Jcc(Lt s32s64 (a %ireg , b %ireg),#l)    "\tcmp {a}, {b}\n\tjl  .L{l}\n"       {1}
+:  Jcc(Le s32s64 (a %ireg , b %ireg),#l)    "\tcmp {a}, {b}\n\tjle .L{l}\n"       {1}
+:  Jcc(Gt s32s64 (a %ireg , b %ireg),#l)    "\tcmp {a}, {b}\n\tjg .L{l}\n"       {1}
+:  Jcc(Ge s32s64 (a %ireg , b %ireg),#l)    "\tcmp {a}, {b}\n\tjge .L{l}\n"       {1}
 
-:  Jcc(Lt p (a %ireg , b %ireg),#l)    "\tcmp {a:.64}, {b:.64}\n\tjb  .L{l}\n"      {1}
-:  Jcc(Le p (a %ireg , b %ireg),#l)    "\tcmp {a:.64}, {b:.64}\n\tjbe .L{l}\n"      {1}
-:  Jcc(Gt p (a %ireg , b %ireg),#l)    "\tcmp {a:.64}, {b:.64}\n\tja  .L{l}\n"      {1}
-:  Jcc(Ge p (a %ireg , b %ireg),#l)    "\tcmp {a:.64}, {b:.64}\n\tjae .L{l}\n"      {1}
+:  Jcc(Lt p (a %ireg , b %ireg),#l)         "\tcmp {a:.64}, {b:.64}\n\tjb  .L{l}\n"      {1}
+:  Jcc(Le p (a %ireg , b %ireg),#l)         "\tcmp {a:.64}, {b:.64}\n\tjbe .L{l}\n"      {1}
+:  Jcc(Gt p (a %ireg , b %ireg),#l)         "\tcmp {a:.64}, {b:.64}\n\tja  .L{l}\n"      {1}
+:  Jcc(Ge p (a %ireg , b %ireg),#l)         "\tcmp {a:.64}, {b:.64}\n\tjae .L{l}\n"      {1}
 
-%ireg:  Cvp (_r %ireg)              #"#extend/truncuate" {2}
-%ireg:  Cvs s64s32s16s8(_r %ireg)   #"#extend/truncuate" {2}
+%ireg:  Cvp (_r %ireg)                  #"#extend/truncuate" {2}
+%ireg:  Cvs s64s32s16s8(_r %ireg)       #"#extend/truncuate" {2}
 
-:       Arg pi32i64(r %ireg)         #"\tpush {r:.64}\n" {1}
-%eax:   Call pi64i32i16i8v(#name)    #"#call {name}\n" {20}
-%eax:   CallV pi64i32i16i8v(r %callv) #"#call {r}\n"    {20}
-:       Call v(#name)                #"#call {name}\n" {20}
-:       CallV v(r %callv)             #"#call {r}\n"    {20}
+:       Arg pi32i64(r %ireg)            #"\tpush {r:.64}\n" {1}
+%eax:   Call pi64i32i16i8v(#name)       #"#call {name}\n" {20}
+%eax:   CallV pi64i32i16i8v(r %callv)   #"#call {r}\n"    {20}
+:       Call v(#name)                   #"#call {name}\n" {20}
+:       CallV v(r %callv)               #"#call {r}\n"    {20}
 }
 
 impl BackendAMD64 {
