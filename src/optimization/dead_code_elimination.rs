1e77a2131aac34612e2388d3b6c3db746e09603f
lotrbuilders <daan.2008@hotmail.com>
2022-02-13 12:25:45 +0100

    Add dead code elimation stage [optimization]

---
+++ src/optimization/dead_code_elimination.rs
@@ -0,0 +1,87 @@
+use super::analysis::ControlFlowGraph;
+use crate::backend::ir::*;
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::mem;
+
+pub fn eliminate_dead_code(function: &mut IRFunction) {
+    let mut cfg = ControlFlowGraph::construct(&function.instructions);
+    let dead_blocks = find_dead_blocks(&mut cfg);
+    remove_blocks(function, &dead_blocks);
+    renumber_blocks(function, &dead_blocks)
+}
+
+fn find_dead_blocks(cfg: &mut ControlFlowGraph) -> HashSet<u32> {
+    let mut work_list: VecDeque<_> = (1..cfg.len() as u32).collect();
+    let mut dead_code = HashSet::new();
+
+    while let Some(block) = work_list.pop_front() {
+        if block == 0 {
+            continue;
+        }
+        if cfg[block].predecessors.is_empty() {
+            dead_code.insert(block);
+            let successors = mem::take(&mut cfg[block].successors);
+            for succ in successors {
+                work_list.push_back(succ);
+                cfg[succ].predecessors.retain(|b| *b != block);
+            }
+        }
+    }
+
+    dead_code
+}
+
+fn remove_blocks(function: &mut IRFunction, dead_blocks: &HashSet<u32>) {
+    let instruction = mem::take(&mut function.instructions);
+    function.instructions = instruction
+        .into_iter()
+        .scan(false, |remove, instruction| {
+            if let &IRInstruction::Label(_, number) = &instruction {
+                *remove = dead_blocks.contains(&number);
+            }
+            if *remove {
+                Some(None)
+            } else {
+                Some(Some(instruction))
+            }
+        })
+        .filter_map(|p| p)
+        .collect();
+}
+
+fn renumber_blocks(function: &mut IRFunction, dead_blocks: &HashSet<u32>) {
+    let label_map: HashMap<u32, u32> = function
+        .instructions
+        .iter()
+        .scan(0, |counter, instruction| {
+            if let &IRInstruction::Label(_, number) = instruction {
+                let kv = (number, *counter);
+                *counter += 1;
+                Some(Some(kv))
+            } else {
+                Some(None)
+            }
+        })
+        .filter_map(|kv| kv)
+        .collect();
+
+    for instruction in &mut function.instructions {
+        match instruction {
+            IRInstruction::Jcc(.., label)
+            | IRInstruction::Jnc(.., label)
+            | IRInstruction::Label(None, label)
+            | IRInstruction::Jmp(label) => *label = label_map[label],
+
+            IRInstruction::Label(Some(phi), label) => {
+                *label = label_map[label];
+                for sources in &mut phi.sources {
+                    sources.retain(|(label, _)| !dead_blocks.contains(&label));
+                    for (label, _) in sources {
+                        *label = label_map[label];
+                    }
+                }
+            }
+            _ => (),
+        }
+    }
+}
--- src/optimization/mod.rs
+++ src/optimization/mod.rs
@@ -1,12 +1,20 @@
 use crate::{backend::ir::IRFunction, options::OptimizationSettings};
 
 pub mod analysis;
+mod dead_code_elimination;
 mod mem2reg;
 mod remove_variable;
 
+use dead_code_elimination as dce;
+
 pub fn optimize(ir_functions: &mut [IRFunction], optimization_settings: &OptimizationSettings) {
     if optimization_settings.optimization_level >= 1 {
-        for function in ir_functions {
+        for function in &mut *ir_functions {
+            dce::eliminate_dead_code(function);
+        }
+    }
+    if optimization_settings.optimization_level >= 1 {
+        for function in &mut *ir_functions {
             mem2reg::mem2reg(function);
         }
     }
